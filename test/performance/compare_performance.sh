88d23cbf9a54af5e4cbcbc0c48776a3daab69acb
Kshitij Chhabra <chhabrk@amazon.com>
2020-04-23 23:24:28 -0700

    Only pop stash in case of uncommitted changes in the workspace

--- test/performance/compare_performance.sh
+++ test/performance/compare_performance.sh
@@ -4,11 +4,14 @@ set -e
 
 REPOSITORY_ROOT="$(git rev-parse --show-toplevel)"
 WORKING_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+HAS_UNCOMMITTED_CHANGES="$(git status --porcelain)"
 
 rm -rf "$REPOSITORY_ROOT/test/.benchmarks"
-git stash -u && git checkout master
+if [[ $HAS_UNCOMMITTED_CHANGES ]]; then git stash -u; fi
+git checkout master
 pytest --benchmark-autosave --benchmark-only performance
 git stash -u && git checkout $WORKING_BRANCH
-git stash pop && git stash pop
-pytest --benchmark-autosave --benchmark-only --benchmark-compare --benchmark-compare-fail=mean:1% performance
+if [[ $HAS_UNCOMMITTED_CHANGES ]]; then git stash pop; fi
+git stash pop
+pytest --benchmark-autosave --benchmark-only --benchmark-compare --benchmark-compare-fail=mean:0.001 performance
 rm -rf "$REPOSITORY_ROOT/test/.benchmarks"
