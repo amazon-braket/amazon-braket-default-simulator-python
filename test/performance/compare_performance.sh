31fba2a59d78dc2baa826c32759b4a7787c2fd65
Kshitij Chhabra <chhabrk@amazon.com>
2020-04-24 00:12:13 -0700

    Change timer to time.process_time

--- test/performance/compare_performance.sh
+++ test/performance/compare_performance.sh
@@ -9,9 +9,9 @@ HAS_UNCOMMITTED_CHANGES="$(git status --porcelain)"
 rm -rf "$REPOSITORY_ROOT/test/.benchmarks"
 if [[ $HAS_UNCOMMITTED_CHANGES ]]; then git stash -u; fi
 git checkout master
-pytest --benchmark-autosave --benchmark-only performance
+pytest --benchmark-autosave --benchmark-only --benchmark-timer='time.process_time' performance
 git stash -u && git checkout $WORKING_BRANCH
 if [[ $HAS_UNCOMMITTED_CHANGES ]]; then git stash pop; fi
 git stash pop
-pytest --benchmark-autosave --benchmark-only --benchmark-compare --benchmark-compare-fail=mean:0.001 performance
+pytest --benchmark-autosave --benchmark-only --benchmark-timer='time.process_time' --benchmark-compare --benchmark-compare-fail=min:5% performance
 rm -rf "$REPOSITORY_ROOT/test/.benchmarks"
