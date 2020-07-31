307e3bfc2cc2b68f94a63594e762b51e01dc2c63
Ava Wang <57644099+avawang1@users.noreply.github.com>
2020-07-31 14:22:58 -0700

    Modifications as per braket repo name changes (#32)

--- doc/conf.py
+++ doc/conf.py
@@ -4,7 +4,7 @@ import datetime
 import pkg_resources
 
 # Sphinx configuration below.
-project = "amazon-braket-default-simulator-python"
+project = "amazon-braket-default-simulator"
 version = pkg_resources.require(project)[0].version
 release = version
 copyright = "{}, Amazon.com".format(datetime.datetime.now().year)
