209a137398683b98ae22364fd0491c0662c779d7
Tim Chen <67017645+yitinche@users.noreply.github.com>
2020-08-17 13:16:19 -0700

    feature: update init.py

--- src/braket/default_simulator/__init__.py
+++ src/braket/default_simulator/__init__.py
@@ -11,10 +11,12 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
-# Execute initialization code in the gate_operations module
-from braket.default_simulator import gate_operations, observables, result_types  # noqa: F401
-from braket.default_simulator.operation import GateOperation, Observable  # noqa: F401
-from braket.default_simulator.simulation import StateVectorSimulation  # noqa: F401
-from braket.default_simulator.simulator import DefaultSimulator  # noqa: F401
+# Execute initialization code in the gate_operations amd noise_operations module
+from braket.default_simulator import gate_operations, noise_operations, observables, result_types  # noqa: F401
+from braket.default_simulator.operation import GateOperation, KrausOperation, Observable  # noqa: F401
+from braket.default_simulator.statevector_simulation import StateVectorSimulation  # noqa: F401
+from braket.default_simulator.densitymatrix_simulation import DensityMatrixSimulation  # noqa: F401
+from braket.default_simulator.statevector_simulator import StateVectorSimulator  # noqa: F401
+from braket.default_simulator.densitymatrix_simulator import DensityMatrixSimulator  # noqa: F401
 
 from ._version import __version__  # noqa: F401
