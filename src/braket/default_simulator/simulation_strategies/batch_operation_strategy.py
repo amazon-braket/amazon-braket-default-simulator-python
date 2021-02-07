074901b8a307c091b4bf0ba5b3785b6833e9c36a
Cody Wang <speller26@gmail.com>
2021-02-07 14:15:54 -0800

    docs

--- src/braket/default_simulator/simulation_strategies/batch_operation_strategy.py
+++ src/braket/default_simulator/simulation_strategies/batch_operation_strategy.py
@@ -24,24 +24,43 @@ from braket.default_simulator.operation import GateOperation
 def apply_operations(
     state: np.ndarray, qubit_count: int, operations: List[GateOperation], batch_size: int
 ) -> np.ndarray:
-    r"""Applies operations to a state vector in batches of size :math:`batch\_size`.
+    r"""Combines consecutive operations on the same targets and applies them in batches.
 
-    :math:`operations` is partitioned into contiguous batches of size :math:`batch\_size` (with
-    remainder). The state vector is treated as a type :math:`(qubit\_count, 0)` tensor, and each
-    operation is treated as a type :math:`(target\_length, target\_length)` tensor (where
-    :math:`target\_length` is the number of targets the operation acts on), and each batch is
-    contracted in an order optimized among the operations in the batch. Larger batches can be
-    significantly faster (although this is not guaranteed), but will use more memory.
+    First, consecutive operations on the same target qubits are combined into single unitary gates.
+
+    For example::
+
+        |0> -[H]-[X]------[YY]-[ZZ]-
+                           |    |
+        |1> -[X]--@--[XX]--|----|---
+                  |   |    |    |
+        |2> -----[X]-[XX]-[YY]-[ZZ]-
+
+    becomes::
+
+        |0> -[X * H]-----------[ZZ * YY]-
+                                   |
+        |1> ---[X]---[XX * CX]-----|-----
+                         |         |
+        |2> ---------[XX * CX]-[ZZ * YY]-
+
+    (the gates are reversed in each group because matrices are multiplied right to left)
+
+    The combined operations are then partitioned into contiguous batches of size ``batch_size``
+    (with remainder). The state vector is treated as a type :math:`(qubit\_count, 0)` tensor,
+    and each operation is treated as a type :math:`(target\_length, target\_length)` tensor
+    (where :math:`target\_length` is the number of targets the operation acts on), and each batch
+    is contracted in an order optimized among the operations in the batch.
 
     For example, if we have a 4-qubit state :math:`S` and a batch with two gates :math:`G1` and
     :math:`G2` that act on qubits 0 and 1 and 1 and 3, respectively, then the state vector after
     applying the batch is :math:`S^{mokp} = S^{ijkl} G1^{mn}_{ij} G2^{op}_{nl}`.
 
     Depending on the batch size, number of qubits, and the number and types of gates, the speed can
-    be more than twice that of applying operations one at a time. Empirically, noticeable
-    performance improvements were observed starting with a batch size of 10, with increasing
-    performance gains up to a batch size of 50. We tested this with 16 GB of memory. For batch sizes
-    greater than 50, consider using an environment with more than 16 GB of memory.
+    be more than twice that of applying operations one at a time, but memory use will increase.
+    Empirically, noticeable performance improvements were observed starting with a batch size of 10,
+    with increasing performance gains up to a batch size of 50. We tested this with 16 GB of memory.
+    For batch sizes greater than 50, consider using an environment with more than 16 GB of memory.
 
     Args:
         state (np.ndarray): The state vector to apply :math:`operations` to, as a type
