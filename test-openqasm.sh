54ed0a79b238b1a13e78b6cfe6149a2dc6b6ebea
Aaron Berdy <berdy@amazon.com>
2022-06-14 15:01:36 -0700

    remove linting/testing scripts and setup.cfg changes

--- test-openqasm.sh
+++
@@ -1,4 +0,0 @@
-coverage run -m pytest test/unit_tests/braket/default_simulator/openqasm/ \
-test/unit_tests/braket/default_simulator/test_openqasm_state_vector_simulator.py
-coverage combine
-coverage report --include="src/braket/default_simulator/openqasm/*",src/braket/default_simulator/openqasm_state_vector_simulator.py
