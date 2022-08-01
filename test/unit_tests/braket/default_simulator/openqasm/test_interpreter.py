c89589392200d2b550f6332f5068f817e70d0d6c
Aaron Berdy <berdy@amazon.com>
2022-08-01 12:35:37 -0700

    fix: support bit strings for input (#96)

--- test/unit_tests/braket/default_simulator/openqasm/test_interpreter.py
+++ test/unit_tests/braket/default_simulator/openqasm/test_interpreter.py
@@ -1193,12 +1193,23 @@ def test_bit_operators():
 def test_input(in_int):
     qasm = """
     input int[8] in_int;
+    input bit[8] in_bit;
     int[8] doubled;
 
     doubled = in_int * 2;
     """
-    context = Interpreter().run(qasm, inputs={"in_int": in_int})
+    in_bit = "10110010"
+    context = Interpreter().run(
+        qasm,
+        inputs={
+            "in_int": in_int,
+            "in_bit": in_bit,
+        },
+    )
     assert context.get_value("doubled") == IntegerLiteral(in_int * 2)
+    assert context.get_value("in_bit") == convert_string_to_bool_array(
+        BitstringLiteral(value=0b10110010, width=8)
+    )
 
 
 def test_output():
