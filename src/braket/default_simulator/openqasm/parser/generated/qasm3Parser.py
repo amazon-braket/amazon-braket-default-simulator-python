0f62098af90c7f249a68c6003d4408e39079ad02
Aaron Berdy <berdy@amazon.com>
2022-08-04 14:34:01 -0700

    fix: rebuild parsers with new antlr version

--- src/braket/default_simulator/openqasm/parser/generated/qasm3Parser.py
+++ src/braket/default_simulator/openqasm/parser/generated/qasm3Parser.py
@@ -1,4 +1,4 @@
-# Generated from qasm3Parser.g4 by ANTLR 4.9
+# Generated from qasm3Parser.g4 by ANTLR 4.8
 # encoding: utf-8
 from antlr4 import *
 from io import StringIO
@@ -629,7 +629,7 @@ class qasm3Parser ( Parser ):
 
     def __init__(self, input:TokenStream, output:TextIO = sys.stdout):
         super().__init__(input, output)
-        self.checkVersion("4.9")
+        self.checkVersion("4.8")
         self._interp = ParserATNSimulator(self, self.atn, self.decisionsToDFA, self.sharedContextCache)
         self._predicates = None
 
