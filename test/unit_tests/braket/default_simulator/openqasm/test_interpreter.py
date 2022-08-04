70fdf1b8edf85e2703cd1d8597bc0c8152a75200
Aaron Berdy <berdy@amazon.com>
2022-08-04 01:05:54 -0700

    fix: remove pytester (#109)

--- test/unit_tests/braket/default_simulator/openqasm/test_interpreter.py
+++ test/unit_tests/braket/default_simulator/openqasm/test_interpreter.py
@@ -1,4 +1,7 @@
+import os
 import re
+import tempfile
+from pathlib import Path
 
 import numpy as np
 import pytest
@@ -1050,51 +1053,174 @@ def test_if():
     assert convert_bool_array_to_string(context.get_value("m")) == "101"
 
 
-def test_include_stdgates(stdgates):
+def test_include_stdgates():
+    with tempfile.TemporaryDirectory() as temp_dir:
+        os.chdir(temp_dir)
+        Path("stdgates.inc").touch()
+        with open("stdgates.inc", "w", encoding="utf-8") as f:
+            f.write(
+                """
+            // OpenQASM 3 standard gate library
+
+// phase gate
+gate p(λ) a { ctrl @ gphase(λ) a; }
+
+// Pauli gate: bit-flip or NOT gate
+gate x a { U(π, 0, π) a; }
+// Pauli gate: bit and phase flip
+gate y a { U(π, π/2, π/2) a; }
+ // Pauli gate: phase flip
+gate z a { p(π) a; }
+
+// Clifford gate: Hadamard
+gate h a { U(π/2, 0, π) a; }
+// Clifford gate: sqrt(Z) or S gate
+gate s a { pow(1/2) @ z a; }
+// Clifford gate: inverse of sqrt(Z)
+gate sdg a { inv @ pow(1/2) @ z a; }
+
+// sqrt(S) or T gate
+gate t a { pow(1/2) @ s a; }
+// inverse of sqrt(S)
+gate tdg a { inv @ pow(1/2) @ s a; }
+
+// sqrt(NOT) gate
+gate sx a { pow(1/2) @ x a; }
+
+// Rotation around X-axis
+gate rx(θ) a { U(θ, -π/2, π/2) a; }
+// rotation around Y-axis
+gate ry(θ) a { U(θ, 0, 0) a; }
+// rotation around Z axis
+gate rz(λ) a { gphase(-λ/2); U(0, 0, λ) a; }
+
+// controlled-NOT
+gate cx a, b { ctrl @ x a, b; }
+// controlled-Y
+gate cy a, b { ctrl @ y a, b; }
+// controlled-Z
+gate cz a, b { ctrl @ z a, b; }
+// controlled-phase
+gate cp(λ) a, b { ctrl @ p(λ) a, b; }
+// controlled-rx
+gate crx(θ) a, b { ctrl @ rx(θ) a, b; }
+// controlled-ry
+gate cry(θ) a, b { ctrl @ ry(θ) a, b; }
+// controlled-rz
+gate crz(θ) a, b { ctrl @ rz(θ) a, b; }
+// controlled-H
+gate ch a, b { ctrl @ h a, b; }
+
+// swap
+gate swap a, b { cx a, b; cx b, a; cx a, b; }
+
+// Toffoli
+gate ccx a, b, c { ctrl @ ctrl @ x a, b, c; }
+// controlled-swap
+gate cswap a, b, c { ctrl @ swap a, b, c; }
+
+// four parameter controlled-U gate with relative phase γ
+gate cu(θ, φ, λ, γ) a, b { p(γ) a; ctrl @ U(θ, φ, λ) a, b; }
+
+// Gates for OpenQASM 2 backwards compatibility
+// CNOT
+gate CX a, b { ctrl @ U(π, 0, π) a, b; }
+// phase gate
+gate phase(λ) q { U(0, 0, λ) q; }
+// controlled-phase
+gate cphase(λ) a, b { ctrl @ phase(λ) a, b; }
+// identity or idle gate
+gate id a { U(0, 0, 0) a; }
+// IBM Quantum experience gates
+gate u1(λ) q { U(0, 0, λ) q; }
+gate u2(φ, λ) q { gphase(-(φ+λ)/2); U(π/2, φ, λ) q; }
+gate u3(θ, φ, λ) q { gphase(-(φ+λ)/2); U(θ, φ, λ) q; }
+            """
+            )
+        qasm = """
+        OPENQASM 3;
+        include "stdgates.inc";
+        """
+        context = Interpreter().run(qasm)
+
+        assert {
+            "p",
+            "x",
+            "y",
+            "z",
+            "h",
+            "s",
+            "sdg",
+            "t",
+            "tdg",
+            "sx",
+            "rx",
+            "ry",
+            "rz",
+            "cx",
+            "cy",
+            "cz",
+            "cp",
+            "crx",
+            "cry",
+            "crz",
+            "ch",
+            "swap",
+            "ccx",
+            "cswap",
+            "cu",
+            "CX",
+            "phase",
+            "cphase",
+            "id",
+            "u1",
+            "u2",
+            "u3",
+        }.issubset(context.gate_table.current_scope.keys())
+        os.chdir("..")
+
+
+def test_adder():
     qasm = """
-    OPENQASM 3;
-    include "stdgates.inc";
-    """
-    context = Interpreter().run(qasm)
 
-    assert {
-        "p",
-        "x",
-        "y",
-        "z",
-        "h",
-        "s",
-        "sdg",
-        "t",
-        "tdg",
-        "sx",
-        "rx",
-        "ry",
-        "rz",
-        "cx",
-        "cy",
-        "cz",
-        "cp",
-        "crx",
-        "cry",
-        "crz",
-        "ch",
-        "swap",
-        "ccx",
-        "cswap",
-        "cu",
-        "CX",
-        "phase",
-        "cphase",
-        "id",
-        "u1",
-        "u2",
-        "u3",
-    }.issubset(context.gate_table.current_scope.keys())
+input uint[4] a_in;
+input uint[4] b_in;
 
+gate majority a, b, c {
+    cnot c, b;
+    cnot c, a;
+    ccnot a, b, c;
+}
 
-def test_adder(adder):
-    circuit = Interpreter().build_circuit("adder.qasm", {"a_in": 1, "b_in": 15}, is_file=True)
+gate unmaj a, b, c {
+    ccnot a, b, c;
+    cnot c, a;
+    cnot a, b;
+}
+
+qubit cin;
+qubit[4] a;
+qubit[4] b;
+qubit cout;
+
+// set input states
+for int[8] i in [0: 3] {
+  if(bool(a_in[i])) x a[i];
+  if(bool(b_in[i])) x b[i];
+}
+
+// add a to b, storing result in b
+majority cin, b[3], a[3];
+for int[8] i in [3: -1: 1] { majority a[i], b[i - 1], a[i - 1]; }
+cnot a[0], cout;
+for int[8] i in [1: 3] { unmaj a[i], b[i - 1], a[i - 1]; }
+unmaj cin, b[3], a[3];
+
+// measure results
+//ans[0] = measure cout;
+//ans[1:4] = measure b[0:3];
+"""
+    circuit = Interpreter().build_circuit(qasm, {"a_in": 1, "b_in": 15})
     simulation = StateVectorSimulation(10, 1, 1)
     simulation.evolve(circuit.instructions)
     assert simulation.probabilities[0b_0_0001_0000_1] == 1
@@ -1315,9 +1441,8 @@ def test_bad_update_values_declaration_size_mismatch():
         Interpreter().run(qasm)
 
 
-def test_gate_qubit_reg(stdgates):
+def test_gate_qubit_reg():
     qasm = """
-    include "stdgates.inc";
     qubit[3] qs;
     qubit q;
 
@@ -1352,9 +1477,8 @@ def test_gate_qubit_reg(stdgates):
     )
 
 
-def test_gate_qubit_reg_size_mismatch(stdgates):
+def test_gate_qubit_reg_size_mismatch():
     qasm = """
-    include "stdgates.inc";
     qubit[3] qs;
     qubit q;
 
@@ -1434,10 +1558,8 @@ def test_undefined_subroutine():
         Interpreter().run(qasm)
 
 
-def test_void_subroutine(stdgates):
+def test_void_subroutine():
     qasm = """
-    include "stdgates.inc";
-
     def flip(qubit q) {
       x q;
     }
@@ -1454,9 +1576,8 @@ def test_void_subroutine(stdgates):
     )
 
 
-def test_array_ref_subroutine(stdgates):
+def test_array_ref_subroutine():
     qasm = """
-    include "stdgates.inc";
     int[16] total_1;
     int[16] total_2;
 
@@ -1480,10 +1601,8 @@ def test_array_ref_subroutine(stdgates):
     assert context.get_value("total_2") == IntegerLiteral(55)
 
 
-def test_subroutine_array_reference_mutation(stdgates):
+def test_subroutine_array_reference_mutation():
     qasm = """
-    include "stdgates.inc";
-
     def mutate_array(mutable array[int[8], #dim = 1] arr) {
         int[16] size = sizeof(arr);
         for int[8] i in [0:size - 1] {
@@ -1507,10 +1626,8 @@ def test_subroutine_array_reference_mutation(stdgates):
     )
 
 
-def test_subroutine_array_reference_const_mutation(stdgates):
+def test_subroutine_array_reference_const_mutation():
     qasm = """
-    include "stdgates.inc";
-
     def mutate_array(const array[int[8], #dim = 1] arr) {
         int[16] size = sizeof(arr);
         for int[8] i in [0:size - 1] {
