336e70ff8ec23936c8dd7df76370f417f3f1eb5b
Aaron Berdy <berdy@amazon.com>
2022-08-04 15:00:57 -0700

    fix: rebuild parsers with new antlr version (#111)

--- src/braket/default_simulator/openqasm/parser/generated/BraketPragmasLexer.py
+++ src/braket/default_simulator/openqasm/parser/generated/BraketPragmasLexer.py
@@ -1,4 +1,4 @@
-# Generated from BraketPragmasLexer.g4 by ANTLR 4.9
+# Generated from BraketPragmasLexer.g4 by ANTLR 4.8
 from antlr4 import *
 from io import StringIO
 from typing.io import TextIO
@@ -957,7 +957,7 @@ class BraketPragmasLexer(Lexer):
 
     def __init__(self, input=None, output:TextIO = sys.stdout):
         super().__init__(input, output)
-        self.checkVersion("4.9")
+        self.checkVersion("4.8")
         self._interp = LexerATNSimulator(self, self.atn, self.decisionsToDFA, PredictionContextCache())
         self._actions = None
         self._predicates = None
