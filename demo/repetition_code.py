be20550ff6aceaa94d307d1a9530e3a7ec69e619
Aaron Berdy <berdy@amazon.com>
2023-05-24 14:09:37 -0700

    mcm kwarg in demo

--- demo/repetition_code.py
+++ demo/repetition_code.py
@@ -52,7 +52,7 @@ program = Program(source=ghz_qasm)
 
 
 num_shots = 100
-result = device.run(program, shots=num_shots)
+result = device.run(program, shots=num_shots, mcm=True)
 
 for i in range(num_shots):
     cf = result["cf"][i]
