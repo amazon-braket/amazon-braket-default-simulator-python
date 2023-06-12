68084ee0b7e05f9fd0ed333fc5794ebcdf690be0
sai_prakash ch <spchit@amazon.com>
2023-06-12 11:28:59 -0700

    feat: Add support for basic gates in cirq

---
+++ src/braket/default_simulator/cirq/program_context.py
@@ -0,0 +1,89 @@
+from typing import Tuple, List
+
+import cirq
+import numpy as np
+from braket.ir.jaqcd.program_v1 import Results
+from cirq import Circuit
+
+from braket.default_simulator.openqasm.program_context import AbstractProgramContext
+
+CIRQ_GATES = {
+    "i": cirq.I,
+    "h": cirq.H,
+    "x": cirq.X,
+    "y": cirq.Y,
+    "z": cirq.Z,
+    "cnot": cirq.CNOT,
+    "cz": cirq.CZ,
+    "s": cirq.S,
+    "t": cirq.T,
+    "cphaseshift": cirq.cphase,
+    "rx": cirq.rx,
+    "ry": cirq.ry,
+    "rz": cirq.rz,
+    "cz": cirq.CZ,
+    "swap": cirq.SWAP,
+    "iswap": cirq.ISWAP,
+    "xx": cirq.XX,
+    "yy": cirq.YY,
+    "zz": cirq.ZZ,
+    "ccnot": cirq.CCNOT,
+    "cswap": cirq.CSWAP,
+    "ccz": cirq.CCZ,
+    "ccx": cirq.CCX,
+    "measure": cirq.MeasurementGate,
+}
+
+
+class ProgramContext(AbstractProgramContext):
+    def __init__(self):
+        super().__init__(Circuit())
+
+    def is_builtin_gate(self, name: str) -> bool:
+        user_defined_gate = self.is_user_defined_gate(name)
+        return name in CIRQ_GATES and not user_defined_gate
+
+    def add_phase_instruction(self, target: Tuple[int], phase_value: int):
+        raise NotImplementedError
+
+    def _gate_accepts_parameters(self, gate_class):
+        return hasattr(gate_class, "parameters") and len(gate_class.parameters) > 0
+
+    def add_gate_instruction(
+        self, gate_name: str, target: Tuple[int], params, ctrl_modifiers: List[int], power: int
+    ):
+        qubits = [cirq.LineQubit(int(qubit)) for qubit in target]
+        target_qubits = qubits[len(ctrl_modifiers) :]
+        control_qubits = qubits[: len(ctrl_modifiers)]
+        try:
+            if params:
+                gate = CIRQ_GATES[gate_name](*params).on(*target_qubits)
+            else:
+                gate = CIRQ_GATES[gate_name](*target_qubits)
+        except KeyError:
+            raise NotImplementedError
+
+        gate = gate.controlled_by(*control_qubits, control_values=ctrl_modifiers)
+        gate = gate**power
+
+        self.circuit.append(gate)
+
+    def add_custom_unitary(
+        self,
+        unitary: np.ndarray,
+        target: Tuple[int],
+    ) -> None:
+        """Add a custom Unitary instruction to the circuit"""
+        raise NotImplementedError
+
+    def add_noise_instruction(self, target, probabilities, noise_instruction):
+        """Add a noise instruction the circuit"""
+        raise NotImplementedError
+
+    def add_result(self, result: Results) -> None:
+        """Add a result type to the circuit"""
+        raise NotImplementedError
+
+    def parse_pragma(self, pragma_body: str):
+        """Parse pragma"""
+        raise NotImplementedError
