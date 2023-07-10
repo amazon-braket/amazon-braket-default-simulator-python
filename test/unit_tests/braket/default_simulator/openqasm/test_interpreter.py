73426816aa90d190a79056b2a5255371aaf3486a
Cody Wang <speller26@gmail.com>
2023-07-10 14:20:04 -0700

    fix: Use op names for noise parsing (#185)
    To avoid implementers having to use default simulator instruction classes

--- test/unit_tests/braket/default_simulator/openqasm/test_interpreter.py
+++ test/unit_tests/braket/default_simulator/openqasm/test_interpreter.py
@@ -1,3 +1,16 @@
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
 import os
 import re
 import tempfile
@@ -7,7 +20,7 @@ import numpy as np
 import pytest
 
 from braket.default_simulator import StateVectorSimulation
-from braket.default_simulator.gate_operations import Hadamard, PauliX, CX
+from braket.default_simulator.gate_operations import CX, Hadamard, PauliX
 from braket.default_simulator.gate_operations import PauliY as Y
 from braket.default_simulator.gate_operations import U, Unitary
 from braket.default_simulator.noise_operations import (
