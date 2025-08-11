f16f48d38a52b4d87382eddf42043bbb483d2fe3
shah-rushil <runshah@amazon.com>
2025-08-11 12:29:18 -0700

    test change

--- test.py
+++ test.py
@@ -1,19 +1,20 @@
-from braket.default_simulator.branched_simulator import BranchedSimulator
+# from braket.default_simulator.branched_simulator import BranchedSimulator
 from collections import Counter, defaultdict
 from braket.ir.openqasm import Program as OpenQASMProgram
 from braket.devices import LocalSimulator
 
 qasm_source = """
 OPENQASM 3.0;
-qubit[9] __qubits__;
-h __qubits__[2];
+qubit[11] __qubits__;
+h __qubits__[10];
 
-cnot __qubits__[2], __qubits__[1];
-
+for int i in [0:9]{
+    cnot __qubits__[10], __qubits__[i];
+}
 """
 
 program = OpenQASMProgram(source=qasm_source, inputs={})
 
-python_result = LocalSimulator("braket_sv_branched_python").run(program, shots=1000).result()
+python_result = LocalSimulator("braket_sv").run(program, shots=1000).result()
 
 print(python_result.measurement_counts)
